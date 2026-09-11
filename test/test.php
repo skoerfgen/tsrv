0f9c9dbea6df049752a9c0cbd8fc82eecb9aa9a1
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-12 00:16:01 +0200

    jo

--- test/test.php
+++ test/test.php
@@ -12,9 +12,9 @@ print_r($ac);
 $key=$ac->generateECKey();
 $ac->loadAccountKey($key);
 print_r($ac->register(true));
-echo "::error title=Account2::Registration unsuccessful\n";
+/*echo "::error title=Account2::Registration unsuccessful\n";
 echo "::notice title=Account::Registration successful\n";
-echo "::warning title=Foo::Missing semicolon or not\n";
+echo "::warning title=Foo::Missing semicolon or not\n";*/
 
 
 $domain_config=array(

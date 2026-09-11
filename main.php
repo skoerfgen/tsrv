6eca7081debcc975e51c09fddf80e728519b088e
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-11 21:13:35 +0200

    ok

--- main.php
+++ main.php
@@ -8,3 +8,12 @@ $arrContextOptions=array(
 );  
 $response=file_get_contents('https://pebble:14000/dir', false, stream_context_create($arrContextOptions));
 var_dump($response);
+
+$arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+);  
+$response=file_get_contents('https://nginx/', false, stream_context_create($arrContextOptions));
+var_dump($response);

68b62c79f9ea4470418cce9dfade903482befc30
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-12 01:31:39 +0200

    ex

--- test/test.php
+++ test/test.php
@@ -19,16 +19,16 @@ echo "::warning title=Foo::Missing semicolon or not\n";*/
 
 
 $domain_config=array(
-	'exampledomain0.net'=>array('challenge'=>'dns-01'),
-	'exampledomain1.net'=>array('challenge'=>'dns-01'),
-	'exampledomain2.net'=>array('challenge'=>'dns-01'),
-	'exampledomain3.net'=>array('challenge'=>'dns-01'),
-	'exampledomain4.net'=>array('challenge'=>'dns-01'),
-	'exampledomain5.net'=>array('challenge'=>'dns-01'),
-	'exampledomain6.net'=>array('challenge'=>'dns-01'),
-	'exampledomain7.net'=>array('challenge'=>'dns-01'),
-	'exampledomain8.net'=>array('challenge'=>'dns-01'),
-	'exampledomain9.net'=>array('challenge'=>'dns-01'),
+	'example.net'=>array('challenge'=>'dns-01'),
+	'example.net'=>array('challenge'=>'dns-01'),
+	'example.net'=>array('challenge'=>'dns-01'),
+	'example.net'=>array('challenge'=>'dns-01'),
+	'example.net'=>array('challenge'=>'dns-01'),
+	'example.net'=>array('challenge'=>'dns-01'),
+	'example.net'=>array('challenge'=>'dns-01'),
+	'example.net'=>array('challenge'=>'dns-01'),
+	'example.net'=>array('challenge'=>'dns-01'),
+	'example.net'=>array('challenge'=>'dns-01'),
 );
 
 $ch=curl_init();

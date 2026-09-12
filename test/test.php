8cf8758b1d761cbf7d3f0db805d539ea8e555f59
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-12 22:14:29 +0200

    ok

--- test/test.php
+++ test/test.php
@@ -6,8 +6,6 @@ use skoerfgen\ACMECert\ACMECert;
 
 $ac=new ACMECert('https://pebble:14000/dir');
 
-
-mkdir('foo');
-file_put_contents('foo/out.txt','it works!');
 print_r($ac);
-echo 'ok';
+
+$key=$ac->generateRSAKey();

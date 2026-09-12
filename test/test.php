26bf7a4cdf40c2ac10b700407ac1c20e57904f71
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-13 00:00:52 +0200

    ok

--- test/test.php
+++ test/test.php
@@ -24,7 +24,73 @@ foreach([1024,2048] as $bits){
 	$ac->log('Generating '.$bits.' bits RSA Key');
 	checkKey($ac->generateRSAKey($bits));	
 }
+$ac->log('Get Terms URL');
+$ac->log($ac->getTermsURL());
 
 
+$domain_config=array(
+	'sub0.example.net'=>array('challenge'=>'dns-01'),
+	'sub1.example.net'=>array('challenge'=>'http-01'),
+);
 
-echo $ac->getTermsURL(),"\n";
+$ch=curl_init();
+
+$handler=function($opts) use ($ac,$ch){
+	switch($opts['config']['challenge']){
+		case 'dns-01':
+			$ac->log('-> SET DNS '.$opts['key'].'.'.' | '.$opts['value']);
+			curl_setopt_array($ch,array(
+				CURLOPT_URL=>'http://challtestsrv:8055/set-txt',
+				CURLOPT_RETURNTRANSFER=>true,
+				CURLOPT_POSTFIELDS=>json_encode(array(
+					'host'=>$opts['key'].'.',
+					'value'=>$opts['value']
+				)),
+			));
+			curl_exec($ch);
+			
+			
+			return function($opts)use($ch,$ac){
+				$ac->log('<- REM DNS '.$opts['key'].'.'.' | '.$opts['value']);
+				curl_setopt_array($ch,array(
+					CURLOPT_URL=>'http://challtestsrv:8055/clear-txt',
+					CURLOPT_RETURNTRANSFER=>true,
+					CURLOPT_POSTFIELDS=>json_encode(array(
+						'host'=>$opts['key'].'.',
+					)),
+				));
+				curl_exec($ch);
+			};
+		break;
+		case 'http-01':
+			$opts['key']=basename($opts['key']);
+			$ac->log('-> SET TXT '.$opts['key'].'.'.' | '.$opts['value']);
+			curl_setopt_array($ch,array(
+				CURLOPT_URL=>'http://challtestsrv:8055/add-http01',
+				CURLOPT_RETURNTRANSFER=>true,
+				CURLOPT_POSTFIELDS=>json_encode(array(
+					'token'=>$opts['key'],
+					'content'=>$opts['value']
+				)),
+			));
+			curl_exec($ch);
+			
+			
+			return function($opts)use($ch,$ac){
+				$ac->log('<- REM TXT '.$opts['key'].'.'.' | '.$opts['value']);
+				curl_setopt_array($ch,array(
+					CURLOPT_URL=>'http://challtestsrv:8055/del-http01',
+					CURLOPT_RETURNTRANSFER=>true,
+					CURLOPT_POSTFIELDS=>json_encode(array(
+						'token'=>$opts['key'],
+					)),
+				));
+				curl_exec($ch);
+			};
+		break;
+	}
+};
+
+$fullchain=$ac->getCertificateChain($ac->generateRSAKey(),$domain_config,$handler,array('group'=>true,'authz_reuse'=>true));
+$ret=$ac->getSAN($fullchain);
+print_r($ret);

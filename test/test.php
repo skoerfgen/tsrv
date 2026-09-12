28a7ef1922df2af880e7b2e594bb2e603319aa52
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-12 23:45:29 +0200

    ok

--- test/test.php
+++ test/test.php
@@ -10,13 +10,23 @@ use skoerfgen\ACMECert\ACMECert;
 $ac=new ACMECert('https://pebble:14000/dir');
 $ac->setLogger(function($txt){echo $txt,"\n";});
 
-if (PHP_VERSION_ID>=70100){
-	$key=$ac->generateECKey();
+
+function checkKey($key)use($ac){
 	$ac->loadAccountKey($key);
+	$ac->register(true);
 }
-$key=$ac->generateRSAKey();
-$ac->loadAccountKey($key);
+
+if (PHP_VERSION_ID>=70100){
+	foreach(['P-256','P-384'] as $curve){
+		$ac->log('Generating '.$curve.' EC Key');
+		checkKey($ac->generateECKey($curve));	
+	}
+}
+foreach([1024,2048] as $bits){
+	$ac->log('Generating '.$bits.' bits RSA Key');
+	checkKey($ac->generateRSAKey($bits));	
+}
 
 
-$ac->register(true);
+
 

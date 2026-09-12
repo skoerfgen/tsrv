2170dcf6e2793e968ef39a6fb7fe1df148625288
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-12 23:30:43 +0200

    ok

--- test/test.php
+++ test/test.php
@@ -8,18 +8,22 @@ require 'ACMECert.php';
 use skoerfgen\ACMECert\ACMECert;
 
 $ac=new ACMECert('https://pebble:14000/dir');
-$keys=array();
 
 
 echo '::group::Generating Keys',"\n";
-
-
 if (PHP_VERSION_ID>=70100){
-	$keys[]=$ac->generateECKey();
+	$key=$ac->generateECKey();
+	echo $key,"\n";
+	$ac->loadAccountKey($key);
 }
-$keys[]=$ac->generateRSAKey();
-
-
-print_r($keys);
-
+$key=$ac->generateRSAKey();
+echo $key,"\n";
+$ac->loadAccountKey($key);
 echo '::endgroup::',"\n";
+
+echo '::group::Register Account',"\n";
+print_r($ac->register(true),true);
+echo '::endgroup::',"\n";
+
+
+

f5b842df0aac3b6f1a733c71a7e53c62d4e95d18
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-12 02:28:23 +0200

    ok

--- test/test.php
+++ test/test.php
@@ -13,12 +13,9 @@ $key=$ac->generateECKey();
 $ac->loadAccountKey($key);
 print_r($ac->register(true));
 
-
 /*echo "::error title=Account2::Registration unsuccessful\n";
 echo "::notice title=Account::Registration successful\n";
 echo "::warning title=Foo::Missing semicolon or not\n";*/
-
-
 
 $domain_config=array(
 	'sub0.example.net'=>array('challenge'=>'dns-01'),

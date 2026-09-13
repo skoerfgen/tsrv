b4290ad320cb694b812a126335411aba284d3f6f
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-13 20:33:30 +0200

    ok

--- test/test.php
+++ test/test.php
@@ -141,6 +141,7 @@ $ac->log(print_r($ret,true));
 $ac->log(print_r($fullchain,true));
 $ac->log('::endgroup::');
 
+
 $ac->log('::group::Split Chain');
 $ac->log(print_r($ac->splitChain(reset($fullchain)),true));
 $ac->log('::endgroup::');

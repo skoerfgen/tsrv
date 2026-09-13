129570e9f04b3b91943cf39b90b96a0fe7f1d457
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-13 03:37:32 +0200

    ok

--- test/test.php
+++ test/test.php
@@ -97,7 +97,7 @@ $handler=function($opts) use ($ac){
 		break;
     case 'tls-alpn-01':
       var_dump($opts);
-			$cert=$ac->generateALPNCertificate($ac->generateRSAKey(),$opts['domain'],$opts['value']);
+			//$cert=$ac->generateALPNCertificate($ac->generateRSAKey(),$opts['domain'],$opts['value']);
       req('add-tlsalpn01',array(
 				'host'=>$opts['domain'],
 				'content'=>$opts['value']			

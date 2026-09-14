5a3e99299593b5a5317ca9b12db14d848ea3653d
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-14 22:37:38 +0200

    ok

--- test/another.php
+++ test/another.php
@@ -41,6 +41,104 @@ foreach([4096,3072,2048] as $k=>$bits){
 }
 
 
+// cert
+
+$domain_config=array(
+	'*.example.net'=>array('challenge'=>'dns-01'),
+	'sub.other.example.net'=>array('challenge'=>'dns-01'),
+	'sub2.other.example.net'=>array('challenge'=>'tls-alpn-01'),
+	'example.net'=>array('challenge'=>'http-01'),
+);
+$domain_config[gethostbyname('challtestsrv')]=array('challenge'=>'http-01');
+
+$handler=function($opts) use ($ac){
+	switch($opts['config']['challenge']){
+		case 'dns-01':
+			$ac->log('-> SET DNS '.$opts['key'].'.'.' | '.$opts['value']);
+			req('set-txt',array(
+				'host'=>$opts['key'].'.',
+				'value'=>$opts['value']
+			));
+			
+			return function($opts)use($ac){
+				$ac->log('<- REM DNS '.$opts['key'].'.'.' | '.$opts['value']);
+				req('clear-txt',array(
+					'host'=>$opts['key'].'.',
+				));
+			};
+		break;
+		case 'http-01':
+			$opts['key']=basename($opts['key']);
+			$ac->log('-> SET TXT '.$opts['key'].'.'.' | '.$opts['value']);
+			setIP(gethostbyname('challtestsrv'));
+			req('add-http01',array(
+				'token'=>$opts['key'],
+				'content'=>$opts['value']
+			));
+	
+			return function($opts)use($ac){
+				$ac->log('<- REM TXT '.$opts['key'].'.'.' | '.$opts['value']);
+				req('del-http01',array(
+					'token'=>$opts['key'],
+				));
+			};
+		break;
+    case 'tls-alpn-01':
+			setIP();
+
+			file_put_contents('some_private_key.pem',$ac->generateRSAKey());
+			$cert=$ac->generateALPNCertificate('file://'.'some_private_key.pem',$opts['domain'],$opts['value']);
+      file_put_contents('alpn_cert.pem',$cert);
+      $resource=proc_open(
+        'node alpn_responder.js some_private_key.pem alpn_cert.pem',
+        array(
+          0=>array('pipe','r'),
+          1=>array('pipe','w')
+        ),
+        $pipes
+      );
+
+
+      $ac->log(fgets($pipes[1]));
+
+      return function($opts) use ($resource,$pipes,$ac){
+        // Stop ALPN Responder
+        fclose($pipes[0]);
+        fclose($pipes[1]);
+        proc_terminate($resource);
+        proc_close($resource);
+				$ac->log('ALPN TERM');
+      };
+    break;
+	}
+};
+
+$fullchain=$ac->getCertificateChains($ac->generateRSAKey(),$domain_config,$handler,array('group'=>true,'authz_reuse'=>true));
+$ret=$ac->getSAN(reset($fullchain));
+$ac->log(print_r($ret,true));
+$ac->log(print_r($fullchain,true));
+
+// ============================================================================
+
+function req($path,$arr){
+	static $ch=null;
+
+	if ($ch===null){
+		$ch=curl_init();
+	}
+	curl_setopt_array($ch,array(
+		CURLOPT_URL=>'http://challtestsrv:8055/'.$path,
+		CURLOPT_RETURNTRANSFER=>true,
+		CURLOPT_POSTFIELDS=>json_encode($arr),
+	));
+	curl_exec($ch);
+}
+
+function setIP($ip=null){
+	req('set-default-ipv4',array(
+		'ip'=>$ip===null?gethostbyname(gethostname()):$ip
+	));	
+}
 
 function open($txt){
 	echo '::group::'.$txt,"\n";

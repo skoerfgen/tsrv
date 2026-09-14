4817a75b8151f7132c26f13ed4d8bab1ad15f11b
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-14 21:26:59 +0200

    ok

---
+++ test/another.php
@@ -0,0 +1,10 @@
+<?php
+
+echo 'PHP Version: '.PHP_VERSION,"\n";
+echo "Own IP: " . gethostbyname(gethostname()) . "\n";
+
+require 'ACMECert.php';
+use skoerfgen\ACMECert\ACMECert;
+$ac=new ACMECert('https://pebble:14000/dir');
+
+print_r($ac);

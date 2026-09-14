717ceefa5d49ba54bec7035c734e551d929f928d
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-14 21:39:30 +0200

    ok

--- test/another.php
+++ test/another.php
@@ -1,10 +1,28 @@
 <?php
 
-echo 'PHP Version: '.PHP_VERSION,"\n";
-echo "Own IP: " . gethostbyname(gethostname()) . "\n";
+// echo 'PHP Version: '.PHP_VERSION,"\n";
+// echo "Own IP: " . gethostbyname(gethostname()) . "\n";
 
 require 'ACMECert.php';
 use skoerfgen\ACMECert\ACMECert;
 $ac=new ACMECert('https://pebble:14000/dir');
+$ac->setLogger(function($txt){
+	echo $txt,"\n";
+});
 
+start('Constructor');
 print_r($ac);
+end();
+
+
+
+
+
+
+
+function start($txt){
+	echo '::group::'.$txt,"\n";
+}
+function end($txt){
+	echo '::endgroup::',"\n";
+}

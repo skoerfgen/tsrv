b698404d45011ca78629844b57c446e013966055
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-15 01:36:44 +0200

    ok

--- test/another.php
+++ test/another.php
@@ -1,7 +1,20 @@
 <?php
 
 echo 'PHP Version: '.PHP_VERSION,"\n";
+
 // echo "Own IP: " . gethostbyname(gethostname()) . "\n";
+
+$hostname = gethostname();
+
+$ipv4 = gethostbyname($hostname);
+
+$records = dns_get_record($hostname, DNS_AAAA);
+$ipv6 = $records[0]['ipv6'] ?? null;
+
+echo "IPv4: $ipv4\n";
+echo "IPv6: $ipv6\n";
+
+exit();
 
 require 'ACMECert.php';
 use skoerfgen\ACMECert\ACMECert;

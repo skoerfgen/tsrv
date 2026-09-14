8fed54f60c364cf653d8f92bfd3dc1d17d4fe196
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-14 19:35:02 +0200

    ok

--- test/test.php
+++ test/test.php
@@ -19,19 +19,6 @@ function checkKey($key){
 	$ac->register(true);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 if (PHP_VERSION_ID>=70100){
 	foreach(['P-256','P-384'] as $curve){
 		$ac->log('::group::Generating '.$curve.' EC Key');

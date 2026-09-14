585c04c7ea61f11dd21f1e77a50989756903be64
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-14 21:06:55 +0200

    ok

--- test/test.php
+++ test/test.php
@@ -12,7 +12,7 @@ use skoerfgen\ACMECert\ACMECert;
 
 $ac=new ACMECert('https://pebble:14000/dir');
 
-function checkKey($key){
+function checkXKey($key){
 	global $ac;
 	$ac->log($key);
 	$ac->loadAccountKey($key);

52f1ddc1bfd93ef6fe04948441b4c409043ded0c
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-14 22:49:44 +0200

    ok

--- test/another.php
+++ test/another.php
@@ -13,7 +13,7 @@ $ac->setLogger(function($txt){
 foreach([2048,3072,4096] as $k=>$bits){
 	open('Generate RSA '.$bits.' Key ('.($k===0?'Register':'Account Key Rollover').')');
 	$key=$ac->generateRSAKey($bits);
-	echo $key,"\n";
+	$ac->log($key);
 	if ($k===0) {
 		$ac->loadAccountKey($key);
 		$ac->register(true);
@@ -28,7 +28,7 @@ if (PHP_VERSION_ID>=70100){
 	foreach(['P-256','P-384','P-521'] as $k=>$curve){
 		open('Generate EC '.$curve.' Key ('.($k===0?'Register':'Account Key Rollover').')');
 		$key=$ac->generateECKey($curve);
-		echo $key,"\n";
+		$ac->log($key);
 		if ($k===0) {
 			$ac->loadAccountKey($key);
 			$ac->register(true);
@@ -113,16 +113,15 @@ $handler=function($opts) use ($ac){
 	}
 };
 
-
 $fullchain=$ac->getCertificateChains($ac->generateRSAKey(),$domain_config,$handler,array('group'=>true,'authz_reuse'=>true));
 $ret=$ac->getSAN(reset($fullchain));
-print_r($ret,true);
-print_r($fullchain,true);
+$ac->log(print_r($ret,true));
+$ac->log(print_r($fullchain,true));
 close();
 
 if (PHP_VERSION_ID>=70201){
 	open('ARI');
-	print_r($ac->getARI(reset($fullchain)),true);
+	$ac->log(print_r($ac->getARI(reset($fullchain)),true));
 	close();
 }
 

c3a20668e663bdf970030d9a376494a4dcef9014
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-15 23:35:35 +0200

    ok

--- src/ACMECert.php
+++ src/ACMECert.php
@@ -33,7 +33,7 @@ use skoerfgen\ACMECert\ACME_Exception;
 use Exception;
 use stdClass;
 
-class ACMECert extends ACMEv2 {
+claXss ACMECert extends ACMEv2 {
 	private $alternate_chains=array();
 
 	public function register($termsOfServiceAgreed=false,$contacts=array()){

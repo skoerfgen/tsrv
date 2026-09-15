5000229f68842aec88cce2a1718a411508cf7dbb
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-15 03:18:46 +0200

    ok

--- test/another.php
+++ test/another.php
@@ -2,7 +2,7 @@
 
 echo 'PHP Version: '.PHP_VERSION,"\n";
 // echo "Own IP: " . gethostbyname(gethostname()) . "\n";
-echo shell_exec('/usr/local/etc/php/');
+echo shell_exec('ls -hal /usr/local/etc/php/');
 exit();
 
 require 'ACMECert.php';

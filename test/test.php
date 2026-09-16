e14347682f8cb51b627e49fe61e78fdca0dda0ab
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-17 01:08:03 +0200

    ok

--- test/test.php
+++ test/test.php
@@ -8,7 +8,7 @@ use skoerfgen\ACMECert\ACMECert;
 
 $ac=new ACMECert('https://127.0.0.1:14000/dir');
 $ac->loadAccountKey($ac->generateRSAKey());
-print_r($ac->registerEAB(true,'kid-1','dzWNDZM6eQGHWpSRTPal5eIUYFTu7EajVIoguysqZ9wG44nMEtx3MUAsUDkMTQ12W'));
+print_r($ac->registerEAB(true,'kid-1','zWNDZM6eQGHWpSRTPal5eIUYFTu7EajVIoguysqZ9wG44nMEtx3MUAsUDkMTQ12W'));
 exit();
 
 

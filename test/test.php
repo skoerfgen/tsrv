701e3e9d082c66eee03721742215c438315421d7
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-17 01:11:48 +0200

    ok

--- test/test.php
+++ test/test.php
@@ -6,11 +6,11 @@ echo 'PHP Version: '.PHP_VERSION,"\n";
 require 'ACMECert.php';
 use skoerfgen\ACMECert\ACMECert;
 
+open('EAB');
 $ac=new ACMECert('https://127.0.0.1:14000/dir');
 $ac->loadAccountKey($ac->generateRSAKey());
 print_r($ac->registerEAB(true,'kid-1','zWNDZM6eQGHWpSRTPal5eIUYFTu7EajVIoguysqZ9wG44nMEtx3MUAsUDkMTQ12W'));
-exit();
-
+close();
 
 $ac=new ACMECert('https://pebble:14000/dir');
 $ac->setLogger(function($txt){

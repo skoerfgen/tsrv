1da9ecfb110765ea073f671da5dc4dec9e285c96
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-16 21:01:26 +0200

    ok

--- test/test.php
+++ test/test.php
@@ -164,7 +164,7 @@ if (PHP_VERSION_ID>=70201){
 
 open('Profiles');
 foreach($ac->getProfiles() as $name=>$description){
-	echo 'Using Profile "'.$name.'" ('.$description.')',"\n"
+	echo 'Using Profile "'.$name.'" ('.$description.')',"\n";
 	$fullchains=$ac->getCertificateChains($ac->generateRSAKey(),$domain_config,$handler,array('profile'=>$name));
 	print_r($fullchains);
 }

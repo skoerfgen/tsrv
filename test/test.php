4dcacba950893fea83be274c2a0f4c61f1aab924
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-17 02:20:36 +0200

    ok

--- test/test.php
+++ test/test.php
@@ -10,13 +10,16 @@ use skoerfgen\ACMECert\ACMECert;
 
 open('EAB');
 $ac=new ACMECert('https://127.0.0.1:14000/dir');
+$ac->setLogger(function($txt){
+	//echo $txt,"\n";
+});
 $ac->loadAccountKey($ac->generateRSAKey());
 print_r($ac->registerEAB(true,'kid-1','zWNDZM6eQGHWpSRTPal5eIUYFTu7EajVIoguysqZ9wG44nMEtx3MUAsUDkMTQ12W'));
 close();
 
 $ac=new ACMECert('https://pebble:14000/dir');
 $ac->setLogger(function($txt){
-	echo $txt,"\n";
+	//echo $txt,"\n";
 });
 
 

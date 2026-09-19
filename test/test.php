02390071ce852a74d9f9036eb574a391e156b214
Stefan Körfgen <skoerfgen@users.noreply.github.com>
2026-09-19 03:48:30 +0200

    ok

--- test/test.php
+++ test/test.php
@@ -3,8 +3,6 @@
 echo 'PHP Version: '.PHP_VERSION,"\n";
 // echo "Own IP: " . gethostbyname(gethostname()) . "\n";
 
-phpinfo();
-
 require 'ACMECert.php';
 use skoerfgen\ACMECert\ACMECert;
 $ac=new ACMECert('https://127.0.0.1:14000/dir');
